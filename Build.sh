010248fd5b7fbb42ab3dc97cca0414c42cada42b
Ingo Oppermann <ingo@datarhei.com>
2025-03-03 12:09:35 +0100

    Upgrade to ffmpeg7.1.1, remove env files

--- Build.sh
+++ Build.sh
@@ -2,28 +2,24 @@
 
 set -au
 
-function source_env() {
-  env=${1:-.env}
-  [ ! -f "${env}" ] && { echo "Env file ${env} doesn't exist"; return 1; }
-  eval $(sed -e '/^\s*$/d' -e '/^\s*#/d' -e 's/=/="/' -e 's/$/"/' -e 's/^/export /' "${env}")
-}
-
 function build_default_native() {
-  source_env ./Build.alpine.env
+  export OS_NAME=alpine
+  export OS_VERSION=3.21
+  export FFMPEG_VERSION=7.1.1
+
   docker build \
     --progress=plain \
     --build-arg BUILD_IMAGE=$OS_NAME:$OS_VERSION \
     --build-arg FFMPEG_VERSION=$FFMPEG_VERSION \
     -f Dockerfile.alpine \
     -t datarhei/base:ffmpeg${FFMPEG_VERSION}-${OS_NAME}${OS_VERSION} .
-  docker tag datarhei/base:ffmpeg${FFMPEG_VERSION}-${OS_NAME}${OS_VERSION} datarhei/base:$OS_NAME-ffmpeg-$OS_VERSION-$FFMPEG_VERSION
 }
 
 function build_default() {
-  source_env ./Build.alpine.env
-  # "--load" does not support multiple platforms
-  # use "--push" to publish
-  # --platform linux/amd64,linux/arm64,linux/arm/v7
+  export OS_NAME=alpine
+  export OS_VERSION=3.21
+  export FFMPEG_VERSION=7.1.1
+
   docker buildx build \
     --load \
     --progress=plain \
@@ -32,37 +28,27 @@ function build_default() {
     --platform linux/amd64 \
     -f Dockerfile.alpine \
     -t datarhei/base:ffmpeg${FFMPEG_VERSION}-${OS_NAME}${OS_VERSION} .
-  docker tag datarhei/base:ffmpeg${FFMPEG_VERSION}-${OS_NAME}${OS_VERSION} datarhei/base:$OS_NAME-ffmpeg-$OS_VERSION-$FFMPEG_VERSION
-  docker tag datarhei/base:ffmpeg${FFMPEG_VERSION}-${OS_NAME}${OS_VERSION} datarhei/base:$OS_NAME-ffmpeg-latest
 }
 
 function build_rpi() {
-  source_env ./Build.alpine.env
-  source_env ./Build.alpine.rpi.env
+  export OS_NAME=alpine
+  export OS_VERSION=3.21
+  export FFMPEG_VERSION=7.1.1
+
   docker build \
     --progress=plain \
     --build-arg BUILD_IMAGE=$OS_NAME:$OS_VERSION \
     --build-arg FFMPEG_VERSION=$FFMPEG_VERSION \
     -f Dockerfile.alpine.rpi \
     -t datarhei/base:ffmpeg${FFMPEG_VERSION}-rpi-${OS_NAME}${OS_VERSION} .
-  docker tag datarhei/base:ffmpeg${FFMPEG_VERSION}-rpi-${OS_NAME}${OS_VERSION} datarhei/base:$OS_NAME-ffmpeg-rpi-$OS_VERSION-$FFMPEG_VERSION
-  docker tag datarhei/base:ffmpeg${FFMPEG_VERSION}-rpi-${OS_NAME}${OS_VERSION} datarhei/base:$OS_NAME-ffmpeg-rpi-latest
-}
-
-function build_cuda11() {
-  source_env ./Build.ubuntu.cuda11.env
-  docker build \
-    --progress=plain \
-    --build-arg BUILD_IMAGE=nvidia/cuda:$CUDA_VERSION-devel-ubuntu$OS_VERSION \
-    --build-arg DEPLOY_IMAGE=nvidia/cuda:$CUDA_VERSION-runtime-ubuntu$OS_VERSION \
-    --build-arg FFNVCODEC_VERSION=$FFNVCODEC_VERSION \
-    --build-arg FFMPEG_VERSION=$FFMPEG_VERSION \
-    -f Dockerfile.ubuntu.cuda11 \
-    -t datarhei/base:ffmpeg${FFMPEG_VERSION}-cuda-ubuntu$OS_VERSION-cuda${CUDA_VERSION} .
 }
 
 function build_cuda12() {
-  source_env ./Build.ubuntu.cuda12.env
+  export OS_NAME=ubuntu
+  export OS_VERSION=24.04
+  export FFMPEG_VERSION=7.1.1
+  export FFNVCODEC_VERSION=12.2.72.0
+
   docker build \
     --progress=plain \
     --build-arg BUILD_IMAGE=nvidia/cuda:$CUDA_VERSION-devel-ubuntu$OS_VERSION \
@@ -74,8 +60,10 @@ function build_cuda12() {
 }
 
 function build_vaapi() {
-  source_env ./Build.ubuntu.env
-  source_env ./Build.ubuntu.vaapi.env
+  export OS_NAME=ubuntu
+  export OS_VERSION=24.04
+  export FFMPEG_VERSION=7.1.1
+
   docker buildx build \
     --load \
     --progress=plain \
@@ -89,7 +77,7 @@ function build_vaapi() {
 
 main() {
   if [[ $# == 0 ]]; then
-    echo "Options available: default, default_native, rpi, cuda11, cuda12, vaapi"
+    echo "Options available: default, default_native, rpi, cuda12, vaapi"
     exit 0
   else
     if [[ $1 == "default" ]]; then
@@ -98,8 +86,6 @@ main() {
       build_default_native
     elif [[ $1 == "rpi" ]]; then
       build_rpi
-    elif [[ $1 == "cuda11" ]]; then
-      build_cuda11
     elif [[ $1 == "cuda12" ]]; then
       build_cuda12
     elif [[ $1 == "vaapi" ]]; then
